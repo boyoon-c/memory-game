3b304374cb056fa4d2b38e4e3cf72a0c880e87a3
boyoon-c <boyoonchang@gmail.com>
2021-07-02 23:14:18 -0700

    minimal skeleton set up

---
+++ js/app.js
@@ -0,0 +1,17 @@
+// Will have 4 by 6 board with total of 8 matches (Each player needs to flip three cards) 
+// Create a board
+
+// Attach event listener for each card on a board that invokes a function of displaying card
+// Shuffle the order of the cards on the board
+// Create shuffle button
+// Attach event listener to the button that invokes a function that uses randomized number to create different combination of a card array
+// Each player flips three cards at a time
+// Create player turn that alternates turns when multiples-of-three clicks are made
+// See if there is a match
+// Assign type to each card 
+// Compare the type of the flipped cards
+// If there is no match: Make all the cards flip downward
+// If there is a match: Remove the matched cards from the board
+// Track each player’s performance by counting the number of matches that a player found
+// Create restart button to allow players to reset the game or reshuffle the cards
+// If players found all the matches, show the message to reveals who is the winner
